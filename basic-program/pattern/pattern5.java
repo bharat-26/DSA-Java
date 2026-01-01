de6a692d86c34a6ce1d7f8b8e7d76c0fa68a76bf
Bharat Choudhary <bharatchoudhary430@gmail.com>
2026-01-02 00:02:41 +0530

    pattern problems

---
+++ basic-program/pattern/pattern5.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class pattern5 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+        for(int i=1; i<=n; i++) {
+            // print empty spaces 
+            for(int j=1; j<=n-i; j++) {
+                System.out.print(" ");
+            }
+
+            // print stars 
+            for(int j=1; j<=i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
